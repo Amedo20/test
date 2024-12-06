fc2e097e96d8f0fb7ba058245b318e773bce4079
Akhmad <example@gmail.com>
2024-12-06 21:49:07 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//дз к 14 уроку:
+let boys = 10;
+let girls = 18;
+let classmates = girls + boys;
+console.log(classmates); //количество одноклассников
+
+
+//- Создать две переменные с правильным именем и две переменные с неправильным:
+let name = 'musa'
+let age = 34;
+
+/* let 1 = 4;
+let true = false;  */
